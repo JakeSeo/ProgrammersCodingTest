e3fbcfcb1009bea91490ceb1aff031a7eb05a1c3
DongSeong Seo <dong.seong.jake.seo@gmail.com>
2021-01-14 22:22:03 +0900

    skeleton code for ExpressWithN....

---
+++ src/dynamic_programming/ExpressWithN.java
@@ -0,0 +1,17 @@
+package dynamic_programming;
+
+import java.util.ArrayList;
+
+public class ExpressWithN {
+    public static int solution(int N, int number) {
+        
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+    	int N = 5;
+    	int number = 12;
+    	
+    	System.out.println(solution(N, number));
+    }
+}
