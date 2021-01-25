86458f15436a0d6c5ebb790f5cbb91dd02615ff5
DongSeong Seo <dong.seong.jake.seo@gmail.com>
2021-01-26 01:13:35 +0900

    3rd test case wrong (cant figure out why yet)

--- src/DFS_BFS/WordConversion.java
+++ src/DFS_BFS/WordConversion.java
@@ -1,6 +1,14 @@
 package DFS_BFS;
 
 public class WordConversion {
+	public static boolean containsWord(String word, String[] words) {
+		for(String w : words) {
+			if(word.equals(w)) {
+				return true;
+			}
+		}
+		return false;
+	}
 	public static boolean oneLetterDifferent(String s1, String s2) {
 		int diff = 0;
 		for(int i = 0; i < s1.length(); i++) {
@@ -18,11 +26,12 @@ public class WordConversion {
 	public static int dfs(int i, String word, String begin, String target, String[] words, boolean[] visited) {
 		if(word.equals(target)) {
 			System.out.println("hi");
-			return 0;
+			return 1;
 		}
 		visited[i] = true;
 		for(int j = 0; j < words.length; j++) {
 			if(!visited[j] && oneLetterDifferent(word, words[j])) {
+				System.out.println(word);
 				return 1 + dfs(j, words[j], begin, target, words, visited);
 			}
 		}
@@ -31,6 +40,9 @@ public class WordConversion {
 	
 	public static int solution(String begin, String target, String[] words) {
 		int answer = 99999;
+		if(!containsWord(target, words)) {
+			return 0;
+		}
 		boolean[] visited = new boolean[words.length];
 		for(int i = 0; i < words.length; i++) {
 			if(oneLetterDifferent(begin, words[i])) {
