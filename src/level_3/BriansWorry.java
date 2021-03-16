e10eeb8848f2a692bd6873889bf086e212d05e4c
DongSeong Seo <dong.seong.jake.seo@gmail.com>
2021-03-16 17:35:21 +0900

    updated brians worry code (still not passed though)

--- src/level_3/BriansWorry.java
+++ src/level_3/BriansWorry.java
@@ -8,64 +8,124 @@ public class BriansWorry {
 	int RULE2 = 1;
 	int RULE_BOTH = 2;
 	int RULE_NOT_SURE = 3;
-	
+
 	public String solution(String sentence) {
 		String answer = "";
 		Stack<Character> small = new Stack<>();
 		Stack<Character> capital = new Stack<>();
-		
-		for(int i = 0; i < sentence.length(); i++) {
+
+		for (int i = 0; i < sentence.length(); i++) {
 			char c = sentence.charAt(i);
-			if(c >= 'a' && c <= 'z') {
+			if (c >= 'a' && c <= 'z') {
 				small.push(c);
 			} else {
 				capital.push(c);
 			}
 		}
 		int i = sentence.length() - 1;
-		ArrayList<Character> charList = new ArrayList<>();
-		int currRule = RULE_NOT_SURE;
-		int count = 0;
-		boolean lastWasSmall = true;
-		while(!small.isEmpty() && !capital.isEmpty()) {
-			char c = sentence.charAt(i);
-			char s;
-			char b;
-			if(c >= 'a' && c <= 'z') {
+		char c = '0';
+		char s = '0';
+		int countS = 0;
+		int countB = 0;
+		while (!small.isEmpty() && i >= 0) {
+			c = sentence.charAt(i);
+			if (small.peek() == c) {
 				s = small.pop();
-				charList.add(s);
-				if(!lastWasSmall) {
-					count = 0;
-				}
-				count++;
-				lastWasSmall = true;
-				if(currRule == RULE_NOT_SURE && count <= 1) {
-					currRule = RULE2;
-				}
+				countS++;
+				i--;
 			} else {
-				b = capital.pop();
-				answer = b + answer;
-				if(lastWasSmall) {
-					count = 0;
+				answer = c + answer;
+				countB++;
+				i--;
+			}
+			if (s != '0') {
+				char start = s;
+				while (!small.isEmpty() && small.peek() == s) {
+					c = sentence.charAt(i);
+					if (c == s) {
+						s = small.pop();
+						countS++;
+						i--;
+					} else {
+						answer = c + answer;
+						countB++;
+						i--;
+					}
 				}
-				count++;
-				lastWasSmall = false;
-				if(currRule == RULE_NOT_SURE && count <= 1) {
-					currRule = RULE1;
+				if (countS == 1 && small.peek() != s) {
+					answer = sentence.charAt(i) + answer;
+					i--;
+					s = small.pop();
+					i--;
+					countS = 1;
+					countB = 1;
+					while (!small.isEmpty() && small.peek() == s) {
+						c = sentence.charAt(i);
+						if (c == s) {
+							s = small.pop();
+							countS++;
+							i--;
+						} else {
+							answer = c + answer;
+							countB++;
+							i--;
+						}
+					}
+					if (small.peek() == start) {
+						small.pop();
+						answer = sentence.charAt(i) + answer;
+						if (i > 0) {
+							answer = " " + answer;
+						}
+						i-=2;
+						countS = 0;
+						countB = 0;
+					}
+					System.out.println(countS + " " + countB);
+				} else {
+					if (countS != 0 && countB == countS) {
+						answer = sentence.charAt(i) + answer;
+						if (i > 0) {
+							answer = " " + answer;
+						}
+						i--;
+						countS = 0;
+						countB = 0;
+					}
+
+					if (countS == 2) {
+						if (i > 0) {
+							answer = " " + answer;
+						}
+						countS = 0;
+						countB = 0;
+					}
 				}
 			}
-			
-			i--;
 		}
 		
-		return answer;
+		int countLetters = 0;
+		for (int j = 0; j < answer.length(); j++) {
+			char character = answer.charAt(j);
+			if (character >= 'A' && character <= 'Z') {
+				countLetters++;
+			}
+		}
+		if (capital.size() == countLetters) {
+			return answer;
+		} else {
+			return "invalid";
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		BriansWorry bw = new BriansWorry();
-		
-		String sentence = "HaEaLaLaObWORLDb";
-		
+
+//		String sentence = "HaEaLaLaOWbObRbLbD";
+		String sentence = "SpIpGpOpNpGJqOqA";
+//		String sentence = "AxAxAxAoBoBoB";
+		// aaaabb
+
 		System.out.println(bw.solution(sentence));
 	}
 }
